7b9cc9b7bb9799408e41f11489bf39ea58df4f53
WOLKEUR <158837911+WOLKEUR@users.noreply.github.com>
2026-02-02 01:07:42 +0100

    Add files via upload

---
+++ testModuleJS.js
@@ -0,0 +1,44 @@
+//COURS 1
+/*
+console.log("%cMessage normal : Ceci est un message normal."); //message normale
+console.warn("Message d'attention : Attention, ceci est un avertissement !"); //avertissement en jeune
+console.error("Message d'erreur : Une erreur s'est produite !");// message derreur en rouge
+*/
+
+
+//COURS 2
+/*
+var : C'est l'étiquette la plus simple. Elle permet de stocker des données qui peuvent être modifiées n'importe où dans votre programme.
+
+let : Cette étiquette est un peu plus restrictive. Elle permet de stocker des données qui ne peuvent être modifiées que dans la partie du programme où elles sont définies.
+    let = "john";
+    on pzut modifier le nom ici 
+    nom = "jane";
+
+const : Cette étiquette est la plus stricte. Elle permet de stocker des données qui ne peuvent pas être modifiées une fois définies.
+
+
+2^53 - 1 limite pour la varible (int) au dela il faut utiliser (BigInt)
+
+
+toUpperCase() : convertit la chaîne en majuscules.
+toLowerCase() : convertit la chaîne en minuscules.
+slice() : extrait une partie de la chaîne.
+charAt() : renvoie le caractère à un indice spécifié.
+indexOf() : renvoie la première occurrence d'une sous-chaîne.
+lastIndexOf() : renvoie la dernière occurrence d'une sous-chaîne.
+
+Et (&&) : Cette opérateur est vrai si et seulement si les deux expressions opérandes sont vraies.
+
+Ou (||) : Cet opérateur est vrai si au moins une des deux expressions opérandes est vraie.
+
+Non (!) : Cet opérateur inverse la valeur booléenne de l'expression opérande.
+*/
+
+const prise = 10;
+const reduct = prise**2;
+console.log(reduct);
+
+
+#.link.server
+
